ac624ae746e72b3f10c6eb744db55e0e18828b6f
eduardomass <37450158+eduardomass@users.noreply.github.com>
2022-04-13 21:12:48 -0300

    Primer Commit

---
+++ Citas/Controllers/TestController.cs
@@ -0,0 +1,32 @@
+﻿using Citas.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Citas.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("Index");
+        }
+
+        public IActionResult Hola()
+        {
+            return View("Chau");
+        }
+
+        //public IActionResult Saludar(string Mensaje, string Nombre)
+        public IActionResult Saludar(Saludo saludo)
+        {
+            //ViewBag.Mensaje = Saludo;
+            //Saludo saludo = new Saludo();
+            //saludo.Mensaje = Mensaje;
+            //saludo.Nombre = Nombre;
+            return View(saludo);
+        }
+    }
+}
---
+++ Citas/Models/Saludo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Citas.Models
+{
+    public class Saludo
+    {
+        public string Mensaje { get; set; }
+
+        public string Nombre { get; set; }
+    }
+}
